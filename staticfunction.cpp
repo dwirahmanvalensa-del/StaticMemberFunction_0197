94412b29cd99a9167f1fe7dd1f82dc6c9b1e7027
dwirahmanvalensa-del <dwirahmanvalensa@gmail.com>
2026-05-04 16:39:59 +0800

    implementasi static function

--- staticfunction.cpp
+++ staticfunction.cpp
@@ -10,4 +10,13 @@ public:
     int id;
     string nama;
 
-    
+    void setID();
+    void printAll();
+
+    static void setNim(int pnim) {
+        nim = pnim;
+    };
+
+    static int getNim() {
+        return nim;
+    }
