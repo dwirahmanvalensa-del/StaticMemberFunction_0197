bd57586f74659847645408e519354937c64b0864
dwirahmanvalensa-del <dwirahmanvalensa@gmail.com>
2026-05-04 16:55:48 +0800

    isi logikan constructor nama

--- constructorpverloading.cpp
+++ constructorpverloading.cpp
@@ -21,3 +21,13 @@ mahasiswa::mahasiswa(int iNim){
     nim = iNim;
 }
 
+mahasiswa::mahasiswa(string iNama){
+    nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama){
+     nim = iNim;
+     nama = iNama;
+}
+
+
